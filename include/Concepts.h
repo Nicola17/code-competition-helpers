2c96a4f018f1ba08ac5f09195f19fe141618cbc5
Nicola17 <nicola.pezzotti@gmail.com>
2014-01-03 01:08:40 +0100

    Add MulticaseSolverConcept

--- include/Concepts.h
+++ include/Concepts.h
@@ -26,6 +26,20 @@ namespace CoCoHe{
 		template <class Stream>
 		static void writeDecoratedOutput(Stream& stream, const std::string& str, int testCase){}
 	};
+	
+	template <class Solver, class Policy>
+	class MulticaseSolverConcept{
+	public:
+		typedef Solver solver_type;
+		typedef typename Solver::Input 	solver_input_type;
+		typedef typename Solver::Output	solver_output_type;
+		typedef typename Solver::Param 	solver_param_type;
+		
+	public:
+		template <class InStream, class OutStream, class LogStream>
+		static void solve(InStream& in, OutStream& out, LogStream& log = 0, const solver_param_type& param = solver_param_type()){}
+	};
+
 
 }
 
